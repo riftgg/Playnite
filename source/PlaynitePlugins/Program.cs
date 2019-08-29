76a88a75592d109d3d718eb6ed7f7cd2bfca9d0c
Víctor Guzmán <vguzman@limitlessgames.com>
2019-08-29 15:05:51 +0200

    List all games, regardless of source

--- source/PlaynitePlugins/Program.cs
+++ source/PlaynitePlugins/Program.cs
@@ -20,29 +20,13 @@ namespace PlaynitePlugins
             GameLibrary gameLibrary = new GameLibrary();
 
             var games = gameLibrary.GetGames();
-            Console.WriteLine("Battle.Net");
-            foreach (var item in games.Where(g => g.Source == "Battle.net"))
+            foreach (var item in games)
             {
-                Console.WriteLine("\t" + item.Name + " " + item.InstallDirectory);
-            }
-            Console.WriteLine("Epic");
-            foreach (var item in games.Where(g => g.Source == "Epic"))
-            {
-                Console.WriteLine("\t" + item.Name + " " + item.InstallDirectory);
-            }
-            Console.WriteLine("Origin");
-            foreach (var item in games.Where(g => g.Source == "Origin"))
-            {
-                Console.WriteLine("\t" + item.Name + " " + item.InstallDirectory);
-            }
-            Console.WriteLine("Steam");
-            foreach (var item in games.Where(g => g.Source == "Steam"))
-            {
-                Console.WriteLine("\t" + item.Name + " " + item.InstallDirectory);
+                Console.WriteLine(item.Source + "\t" + item.Name + " " + item.InstallDirectory);
             }
 
-            string selected = null;
-            int index = -1;
+            string selected;
+            int index;
             do
             {
                 selected = Console.ReadLine();
